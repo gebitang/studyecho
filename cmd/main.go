b17ec187e7a406bb09535aa820c75f3070ba7c2b
gebitang <am@gebitang.com>
2020-08-14 18:16:03 +0800

    render template basic

--- cmd/main.go
+++ cmd/main.go
@@ -4,13 +4,30 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
+	"html/template"
+
+	"io"
 	"net/http"
 	"os"
 	"studyecho/logger"
 )
 
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
 func main() {
 	e := echo.New()
+
+	tpl := &Template{
+		templates: template.Must(template.ParseGlob("template/*.html")),
+	}
+
+	e.Renderer = tpl
 
 	e.Logger = logger.New(os.Stdout)
 	e.Logger.SetLevel(log.DEBUG)
@@ -24,6 +41,11 @@ func main() {
 		zerolog.Debug().Str("path", ctx.Path()).Msg("this is zerolog debug msg")
 
 		return ctx.HTML(http.StatusOK, "hello world")
+	})
+
+	e.GET("/render", func(ctx echo.Context) error {
+
+		return ctx.Render(http.StatusOK, "key", "gebitang")
 	})
 
 	e.Logger.Fatal(e.Start(":2222"))
